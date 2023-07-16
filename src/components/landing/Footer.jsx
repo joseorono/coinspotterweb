b3be56324b69a44656789b2187ed7e657b71122b
José Miguel Oroño <joseomaker@gmail.com>
2023-07-16 17:57:43 -0500

    Update Footer.jsx

--- src/components/landing/Footer.jsx
+++ src/components/landing/Footer.jsx
@@ -2,44 +2,51 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import LOGO from "public/logos/cs_white_outline.png";
+import UnderlineLink from "../links/UnderlineLink";
 
 const Footer = () => {
   return (
-    <div className="flex justify-around bg-[#3a3a3c] pt-12">
-      <div className="flex flex-col justify-evenly">
-        <Link href="/">
-          <li className="list-none py-4 pr-6 text-center text-xs text-white underline">
-            How it Works
-          </li>
-        </Link>
-        <Link href="/">
-          <li className="list-none py-4 pr-6 text-center text-xs text-white underline">
-            How it Works
-          </li>
-        </Link>
+    <footer className="bg-[#3a3a3c] pt-12">
+      <div className="flex justify-around ">
+        <div className="flex flex-col justify-evenly">
+          <Link href="/">
+            <li className="list-none py-4 pr-6 text-center text-xs text-white underline">
+              How it Works
+            </li>
+          </Link>
+          <Link href="/">
+            <li className="list-none py-4 pr-6 text-center text-xs text-white underline">
+              How it Works
+            </li>
+          </Link>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src={LOGO} width={150} height={150} alt="/" />
+        </div>
+        <ul className="flex flex-col justify-evenly">
+          <Link href="/">
+            <li className="py-4 pr-6 text-center text-xs text-white underline">
+              How it Works
+            </li>
+          </Link>
+          <Link href="/">
+            <li className="py-4 pr-6 text-center text-xs text-white underline">
+              Terms of Use
+            </li>
+          </Link>
+        </ul>
       </div>
-      <div className="flex flex-col items-center">
-        <Image src={LOGO} width="64px" height="64px" alt="/" />
-        <Link href="/">
-          <li className="list-none py-4 pr-6 text-center text-xs text-white">
-            @ 2023 COINSPOTTER By Jose Miguel Oroño, Mauricio Garcia y Pedro
-            Zavala
-          </li>
-        </Link>
+      <div>
+        <hr className="m-auto my-4 w-[90%]" />
+        <p className="text-center text-xs text-white p-2">
+          © {new Date().getFullYear()} By{' '}
+          <UnderlineLink href='/'>
+            Jose Miguel Oroño, Mauricio Garcia y Pedro Zavala
+          </UnderlineLink>
+        </p>
       </div>
-      <ul className="flex flex-col justify-evenly">
-        <Link href="/">
-          <li className="py-4 pr-6 text-center text-xs text-white underline">
-            How it Works
-          </li>
-        </Link>
-        <Link href="/">
-          <li className="py-4 pr-6 text-center text-xs text-white underline">
-            Terms of Use
-          </li>
-        </Link>
-      </ul>
-    </div>
+    </footer>
+    
   );
 };
 
