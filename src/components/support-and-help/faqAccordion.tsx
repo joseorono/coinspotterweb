c7fd47b324c1fc739b51e88ba0b98363e7290d17
José Miguel Oroño <joseomaker@gmail.com>
2023-08-20 04:01:27 -0500

    Fix faqAccordion.tsx toggling

--- src/components/support-and-help/faqAccordion.tsx
+++ src/components/support-and-help/faqAccordion.tsx
@@ -2,21 +2,28 @@ import React from 'react';
 
 const FaqAccordion = (props: FaqQuestionsProps) => {
   return (
-    <div className="accordion">
-      {props.questions.map((entry, index: number) => (
-        <div className="accordion-item" key={index}>
-          <h2 className="accordion-header">
-            <button className="accordion-button" type="button">
-              {entry.question}
-            </button>
-          </h2>
-          <div className="accordion-content">
-            <p>{entry.answer}</p>
-          </div>
+    <>
+      <div>
+        <button className="btn btn-primary">Daisy Button</button>
+      </div>
+      <div className="accordion">
+        {props.questions.map((entry, index: number) => (
+        <div className="collapse collapse-plus bg-base-200" key={"faq_item_" + index}>
+            <input type="radio" name="faq_accordion_radio" defaultChecked={index===0} />
+            <div className="collapse-title text-xl font-medium">
+                {entry.question}
+            </div>
+            <div className="collapse-content">
+              <p>{entry.answer}</p>
+            </div>
         </div>
-      ))}
-    </div>
+        
+        ))}
+      </div>
+      
+    </>
   );
 };
 
 export default FaqAccordion;
+
