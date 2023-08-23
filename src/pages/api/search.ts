4f9b6854d00a3e42e29b1b098d4efe7fe2b0ef3f
pedro <pedrozav@gmail.com>
2023-08-23 18:01:56 -0400

    add palces payment an products

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,8 +1,6 @@
 //import { Post, User } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
-
 import { PrismaClient } from "../../../prisma/generated/prisma-client-js";
-
 
 const prisma = new PrismaClient();
 
@@ -19,11 +17,31 @@ export default async function handler(
       }
 
       const productos = await prisma.$queryRaw`
-      SELECT * FROM Producto
-      WHERE LOWER(nombre) LIKE LOWER(CONCAT('%', ${query}, '%'))
-    `;
+        SELECT * FROM Producto
+        WHERE LOWER(nombre) LIKE LOWER(CONCAT('%', ${query}, '%'))
+      `;
 
-      res.status(200).json({ productos });
+      const places = await prisma.$queryRaw`
+        SELECT * FROM Places
+        WHERE LOWER(name) LIKE LOWER(CONCAT('%', ${query}, '%'))
+          OR LOWER(address) LIKE LOWER(CONCAT('%', ${query}, '%'))
+      `;
+
+      const paymentMethodsAccepted = await prisma.$queryRaw`
+        SELECT * FROM payment_methods_accepted
+        WHERE paymethod_id IN (
+          SELECT id FROM payment_methods
+          WHERE LOWER(name) LIKE LOWER(CONCAT('%', ${query}, '%'))
+        )
+      `;
+
+      const paymentMethods = await prisma.$queryRaw`
+        SELECT * FROM payment_methods
+        WHERE LOWER(name) LIKE LOWER(CONCAT('%', ${query}, '%'))
+          OR LOWER(symbol) LIKE LOWER(CONCAT('%', ${query}, '%'))
+      `;
+
+      res.status(200).json({ productos, places, paymentMethodsAccepted, paymentMethods });
     } catch (error: any) {
       console.error(error);
       res.status(500).end();
