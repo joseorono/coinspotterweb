b39097b31063fef610b63a70816c893088cd036c
pedro <pedrozav@gmail.com>
2023-09-01 18:45:35 -0400

    modify and aplly filters and tabs in search

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import Spinner from "./spinner";
 import Link from "next/link";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 interface SearchResult {
-  id: number; // Update with the actual type for your IDs
-  nombre?: string; // Update with the actual properties you want to display
+  id: number; // Actualiza con el tipo real para tus IDs
+  nombre?: string; // Actualiza con las propiedades que desees mostrar
   name?: string;
   address?: string;
-  // Add more properties as needed for other models
+  // Agrega más propiedades según sea necesario para otros modelos
 }
 
 const fetchData = async (url: string) => {
@@ -31,6 +34,27 @@ const SearchPage = () => {
 
   const handleBack = () => {
     router.push("/").catch(console.error);
+  };
+
+  const [selectedProductFilter, setSelectedProductFilter] = useState("");
+  const [selectedPlaceFilter, setSelectedPlaceFilter] = useState("");
+  const [selectedAcceptedFilter, setSelectedAcceptedFilter] = useState("");
+  const [selectedMethodFilter, setSelectedMethodFilter] = useState("");
+
+  const handleApplyProductFilter = () => {
+    // Lógica de filtrado para Productos
+  };
+
+  const handleApplyPlaceFilter = () => {
+    // Lógica de filtrado para Places
+  };
+
+  const handleApplyAcceptedFilter = () => {
+    // Lógica de filtrado para Payment Methods Accepted
+  };
+
+  const handleApplyMethodFilter = () => {
+    // Lógica de filtrado para Payment Methods
   };
 
   if (!encodedSearchQuery) {
@@ -77,50 +101,122 @@ const SearchPage = () => {
       <span className="text-xl">
         Showing results for: <span className="font-semibold">{searchQuery}</span>
       </span>
-      <div>
-        <h2>Productos</h2>
-        {productos.length === 0 ? (
-          <p>No products found.</p>
-        ) : (
-          productos.map((producto: SearchResult) => (
-            <div key={producto.id}>{producto.nombre}</div>
-          ))
-        )}
-      </div>
-      <div>
-        <h2>Places</h2>
-        {places.length === 0 ? (
-          <p>No places found.</p>
-        ) : (
-          places.map((place: SearchResult) => (
-            <div key={place.id}>{place.name}</div>
-          ))
-        )}
-      </div>
-      <div>
-        <h2>Payment Methods Accepted</h2>
-        {paymentMethodsAccepted.length === 0 ? (
-          <p>No payment methods accepted found.</p>
-        ) : (
-          paymentMethodsAccepted.map((method: SearchResult) => (
-            <div key={method.id}>{method.name}</div>
-          ))
-        )}
-      </div>
-      <div>
-        <h2>Payment Methods</h2>
-        {paymentMethods.length === 0 ? (
-          <p>No payment methods found.</p>
-        ) : (
-          paymentMethods.map((method: SearchResult) => (
-            <div key={method.id}>{method.name}</div>
-          ))
-        )}
-      </div>
+      <Tabs>
+        <TabList>
+          <Tab>Productos</Tab>
+          <Tab>Places</Tab>
+          <Tab>Payment Methods Accepted</Tab>
+          <Tab>Payment Methods</Tab>
+        </TabList>
+
+        <TabPanel>
+          <div className="mb-4">
+            <select
+              value={selectedProductFilter}
+              onChange={(e) => setSelectedProductFilter(e.target.value)}
+            >
+              <option value="">All Products</option>
+              <option value="category1">Category 1</option>
+              <option value="category2">Category 2</option>
+              {/* Agregar más opciones de filtro según sea necesario */}
+            </select>
+            <button
+              onClick={handleApplyProductFilter}
+              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded ml-2"
+            >
+              Apply Filter
+            </button>
+          </div>
+          {productos.length === 0 ? (
+            <p>No products found.</p>
+          ) : (
+            productos.map((producto: SearchResult) => (
+              <div key={producto.id}>{producto.nombre}</div>
+            ))
+          )}
+        </TabPanel>
+        <TabPanel>
+          <div className="mb-4">
+            <select
+              value={selectedPlaceFilter}
+              onChange={(e) => setSelectedPlaceFilter(e.target.value)}
+            >
+              <option value="">All Places</option>
+              <option value="place1">Place 1</option>
+              <option value="place2">Place 2</option>
+              {/* Agregar más opciones de filtro según sea necesario */}
+            </select>
+            <button
+              onClick={handleApplyPlaceFilter}
+              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded ml-2"
+            >
+              Apply Filter
+            </button>
+          </div>
+          {places.length === 0 ? (
+            <p>No places found.</p>
+          ) : (
+            places.map((place: SearchResult) => (
+              <div key={place.id}>{place.name}</div>
+            ))
+          )}
+        </TabPanel>
+        <TabPanel>
+          <div className="mb-4">
+            <select
+              value={selectedAcceptedFilter}
+              onChange={(e) => setSelectedAcceptedFilter(e.target.value)}
+            >
+              <option value="">All Payment Methods Accepted</option>
+              <option value="method1">Method 1</option>
+              <option value="method2">Method 2</option>
+              {/* Agregar más opciones de filtro según sea necesario */}
+            </select>
+            <button
+              onClick={handleApplyAcceptedFilter}
+              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded ml-2"
+            >
+              Apply Filter
+            </button>
+          </div>
+          {paymentMethodsAccepted.length === 0 ? (
+            <p>No payment methods accepted found.</p>
+          ) : (
+            paymentMethodsAccepted.map((method: SearchResult) => (
+              <div key={method.id}>{method.name}</div>
+            ))
+          )}
+        </TabPanel>
+        <TabPanel>
+          <div className="mb-4">
+            <select
+              value={selectedMethodFilter}
+              onChange={(e) => setSelectedMethodFilter(e.target.value)}
+            >
+              <option value="">All Payment Methods</option>
+              <option value="method1">Method 1</option>
+              <option value="method2">Method 2</option>
+              {/* Agregar más opciones de filtro según sea necesario */}
+            </select>
+            <button
+              onClick={handleApplyMethodFilter}
+              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded ml-2"
+            >
+              Apply Filter
+            </button>
+          </div>
+          {paymentMethods.length === 0 ? (
+            <p>No payment methods found.</p>
+          ) : (
+            paymentMethods.map((method: SearchResult) => (
+              <div key={method.id}>{method.name}</div>
+            ))
+          )}
+        </TabPanel>
+      </Tabs>
     </>
   );
 };
 
 export default SearchPage;
-
 
