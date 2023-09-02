0de8127f853502c9c228aead6a78cf88a849640d
José Miguel Oroño <joseomaker@gmail.com>
2023-09-02 08:17:00 -0500

    Create new API routers

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
 import { exampleRouter } from "~/server/api/routers/example";
+import { searchRouter } from "~/server/api/routers/search";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { favoritePlacesRouter } from "./routers/favoritePlaces";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +10,8 @@ import { createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  search: searchRouter, // coinspotter.com/api/auth
+  favplaces: favoritePlacesRouter, // coinspotter.com/api/auth
 });
 
 // export type definition of API
