86eb5fe00d6c34e2de550c7cc9a4ef6eb140ea26
José Miguel Oroño <joseomaker@gmail.com>
2023-09-03 18:41:58 -0500

    Create gmapEmbed.tsx

---
+++ src/components/gmaps/gmapEmbed.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from 'react';
+
+
+const GoogleMapEmbed: FC<GoogleMapEmbedProps> = ({
+  width = 600,
+  height = 450,
+  locationQuery,
+  latitude,
+  longitude,
+}) => {
+
+    if ((!locationQuery && (!latitude || !longitude)) || (locationQuery && (latitude || longitude))) {
+        throw new Error("You must provide either 'locationQuery' or both 'latitude' and 'longitude', but not both.");
+    }
+
+    const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+    let src = "";
+    if (locationQuery) {
+        src = `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${encodeURIComponent(
+            locationQuery
+        )}`;
+    } else if (latitude && longitude) {
+        src = `https://www.google.com/maps/embed/v1/view?key=${apiKey}&center=${latitude},${longitude}&zoom=14`;
+    } else {
+        throw new Error("Neither 'locationQuery' nor both 'latitude' and 'longitude' provided.");
+    }
+
+    return (
+        <div className="google-maps-embed">
+            <iframe
+                width={width}
+                height={height}
+                style={{ border: 0 }}
+                loading="lazy"
+                allowFullScreen
+                referrerPolicy="no-referrer-when-downgrade"
+                src={src}
+            />
+        </div>
+    );
+};
+
+export default GoogleMapEmbed;
