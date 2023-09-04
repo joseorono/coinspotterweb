83bf4a099e4b37d0ef4aa896dd6418dafd0786a3
pedro <pedrozav@gmail.com>
2023-09-04 18:37:21 -0400

    referesh search bar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,14 +1,24 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
 import { useState } from 'react';
 
 const SearchInput = () => {
-  const search = useSearchParams();
-  const [searchQuery, setSearchQuery] = useState<string | null>(
-    search ? search.get("q") : ""
-  );
   const router = useRouter();
+  const [searchQuery, setSearchQuery] = useState<string | null>(
+    router.query.q ? router.query.q.toString() : ""
+  );
+  const [selectedCategory, setSelectedCategory] = useState("All categories");
+
+  const categories = [
+    "All categories",
+    "Places",
+    "Places imagen",
+    "Payment methods",
+    "Currencies",
+    "Payment methods accepted",
+    "Productos", 
+  ];
 
   const onSearch = (event: React.FormEvent) => {
     event.preventDefault();
@@ -18,19 +28,52 @@ const SearchInput = () => {
     }
 
     const encodedSearchQuery = encodeURI(searchQuery);
-    router.push(`/search?q=${encodedSearchQuery}`);
+    router.push(`/search?q=${encodedSearchQuery}&category=${selectedCategory}`);
   };
 
   return (
     <form onSubmit={onSearch} className="flex justify-center">
+      <select
+        value={selectedCategory}
+        onChange={(e) => setSelectedCategory(e.target.value)}
+        className="px-2 py-1 mr-2 text-sm text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+      >
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
       <input
         value={searchQuery || ""}
         onChange={(event) => setSearchQuery(event.target.value)}
         className="px-5 py-1 w-full sm:px-5 sm:py-3 flex-1 text-zinc-200 bg-zinc-800 focus:bg-black rounded-full focus:outline-none focus:ring-[1px] focus:ring-green-700 placeholder:text-zinc-400"
         placeholder="¿Qué Estás Buscando?"
       />
+      <button
+        type="submit"
+        className="p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        <svg
+          className="w-4 h-4"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 20 20"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+          />
+        </svg>
+        <span className="sr-only">Search</span>
+      </button>
     </form>
   );
 };
 
 export default SearchInput;
+
