6515bd805ce672de97c36a635ea0cbc0bc628e69
José Miguel Oroño <joseomaker@gmail.com>
2023-09-04 18:27:07 -0500

    Index.tsx: Use correct Hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
 
 
   return (
-    <PageLayout pageTitle="CoinSpotter">
+    <PageLayout pageTitle="CoinSpotter" useHomePageHeader={true}>
       <main>
       <PayMethods />
         <section className='bg-cream bg-white'>
