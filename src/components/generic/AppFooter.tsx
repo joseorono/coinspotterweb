78d0e26fc49ed80725a7171c416c4966aab750bd
José Miguel Oroño <joseomaker@gmail.com>
2023-09-08 07:04:32 -0500

    Create AppFooter.tsx

---
+++ src/components/generic/AppFooter.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AppFooter: React.FC = () => {
+  return (
+    <div className="relative flex py-5 items-center">
+        <div className="flex-grow border-t border-gray-400"></div>
+        <span className="flex-shrink mx-4 text-gray-400">Copyright &copy; {new Date().getFullYear()} CoinSpotter Team - All Rights Reserved.</span>
+        <div className="flex-grow border-t border-gray-400"></div>
+    </div>
+  );
+};
+
+export default AppFooter;
