ec475052dd262e009d255ccaa7ce8f4131fc9aa3
José Miguel Oroño <joseomaker@gmail.com>
2023-09-09 07:21:19 -0500

    Add proper links to AppNavbar and fix logo

--- src/components/generic/AppNavbar.tsx
+++ src/components/generic/AppNavbar.tsx
@@ -3,9 +3,16 @@ import Link from 'next/link';
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import SearchBar from '~/components/SearchInput';
 import Image from 'next/image';
-import csLogo from '@/logos/cs_logo.png';
+import csLogo from '@/logos/cs_white_outline.png';
+import placeholderPfp from '@/placeholder/karamaloran.jpg';
+
 
 // import { logo } from '../assets';
+function getRandomInt(min:number, max:number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 const AppNavBar = () => {
   const [nav, setNav] = useState(false);
@@ -45,9 +52,27 @@ const AppNavBar = () => {
               </svg>
             </label>
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-5 p-2 shadow-lg bg-base-100 rounded-box w-52">
-              <li><a>Homepage</a></li>
-              <li><a>Portfolio</a></li>
-              <li><a>About</a></li>
+           
+              <li>
+                <Link href={`/app/dashboard`}>     
+                  Dashboard
+                </Link>
+              </li>
+              <li>
+                <Link href={`/places/${getRandomInt(1,3)}`}>     
+                    Random Place
+                </Link>
+              </li>
+              <li>
+                <Link href={`/app/favorites`}>     
+                    Favorites
+                </Link>
+              </li>
+              <li>
+                <Link href={`/app/search`}>     
+                    Search
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
@@ -62,6 +87,32 @@ const AppNavBar = () => {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
           </label>
+        </div>
+
+        {/* User Button */}
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <Image src={placeholderPfp} width={40} height={40} alt='User' />
+            </div>
+          </label>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <li>
+              <a href='javascript:void(0);' className="justify-between">
+                Profile
+                <span className="badge">Coming Soon</span>
+              </a>
+            </li>
+            <li>
+              <a href='javascript:void(0);' className="justify-between">
+                Settings
+                <span className="badge">Coming Soon</span>
+              </a>
+            </li>
+            <li>
+              <Link href='/'>Logout</Link>
+            </li>
+          </ul>
         </div>
       </div>
       {/* End-Navbar */}
