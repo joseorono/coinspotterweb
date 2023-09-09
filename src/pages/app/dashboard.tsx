c3f3e389d10ef5162dc8db6dad6ef853f5ddd06c
Mauricio Garcia <78531537+Maggnoone@users.noreply.github.com>
2023-09-09 08:09:55 -0400

    Add styles to the dashboard page

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -5,68 +5,71 @@ import TitleCard from "~/components/dashboard/TitleCard";
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 import SmallStatsCard from "~/components/dashboard/SmallStatsCard";
 import { BsFillBagFill } from "react-icons/bs";
+import AppLayout from "~/components/layout/AppLayout";
 
 export const getServerSideProps: GetServerSideProps<{
-    statsData: ISmallStatsCardProps[]
-  }> = async () => {
+  statsData: ISmallStatsCardProps[];
+}> = async () => {
+  const statsData: ISmallStatsCardProps[] = [
+    {
+      title: "Next.js",
+      value: "1.9k",
+      description: "Number of Users",
+      colorIndex: 0,
+    },
+    {
+      title: "Year",
+      value: "2023",
+      description: "Number of Users",
+      colorIndex: 0,
+    },
+  ];
+  return { props: { statsData } };
+};
 
-    const statsData: ISmallStatsCardProps[] = [
-        {
-            title: 'Next.js',
-            value: "1.9k",
-            description: "Number of Users",
-            colorIndex: 0,
-        },
-        {
-            title: 'Year',
-            value: "2023",
-            description: "Number of Users",
-            colorIndex: 0,
-        },
+export default function Dashboard(
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
+  //export default function Dashboard(props:any) {
+  //const { data: session } = useSession();
+  const { data: session, status } = useSession();
+  console.log("props", props);
+  const { statsData } = props;
 
-    ];
-    return { props: { statsData } }
-}
-
-
-
-export default function Dashboard(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
-//export default function Dashboard(props:any) {
-    //const { data: session } = useSession();
-    const { data: session, status } = useSession();
-    console.log("props", props);
-    const { statsData } = props;
-   
-    return (
-        <>
-        <div className="grid lg:grid-cols-4 mt-2 md:grid-cols-2 grid-cols-1 gap-6">
+  return (
+    <AppLayout pageTitle="CoinSpotter">
+      <div className="mt-2 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
         <BsFillBagFill />
-        {
-            
-            statsData.map((d, k) => {
-                return (
-                    <SmallStatsCard key={k} {...d} colorIndex={k}/>
-                )
-            })
-            
-        }
-        </div>
+        {statsData.map((d, k) => {
+          return <SmallStatsCard key={k} {...d} colorIndex={k} />;
+        })}
+      </div>
 
-        <div className="flex justify-center items-center h-screen bg-gray-100">
-            <div className="space-y-4">
+      <div className="flex items-center justify-center">
+        <div className="mb-10 flex min-w-[992px] flex-row gap-4 space-y-4">
+          <div className="mt-6 w-[20%]">
             <TitleCard title="Welcome" topMargin="mt-4">
-                <div className="text-xl font-semibold">Hello, {session?.user?.name || 'Guest'}!</div>
+              <div className="text-lg font-semibold">
+                Hello, {session?.user?.name || "Guest"}!
+              </div>
             </TitleCard>
+          </div>
+          <div className="mt-10 w-[30%]">
             <TitleCard title="Number of Users">
-                <div className="text-xl font-semibold">Number of Users: {100}</div>
+              <div className="text-lg font-semibold">
+                Number of Users: {100}
+              </div>
             </TitleCard>
+          </div>
+          <div className="mt-10 w-[40%]">
             <TitleCard title="Favorite Places">
-                <div className="text-xl font-semibold">Favorite Places: {5}</div>
+              <div className="text-lg font-semibold">Favorite Places: {5}</div>
             </TitleCard>
-            </div>
+          </div>
         </div>
-        </>
-    );
+      </div>
+    </AppLayout>
+  );
 }
 
 /*
