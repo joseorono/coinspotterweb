78e871ddf078558eee02a3294d38332318a8ec44
pedro <pedrozav@gmail.com>
2023-09-13 19:41:02 -0400

    change form in port

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -97,7 +97,8 @@ export const authOptions: NextAuthOptions = {
     EmailProvider({
       server: {
         host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
+        port: parseInt(process.env.EMAIL_SERVER_PORT || '587'),
+        secure: true,
         auth: {
           user: process.env.EMAIL_SERVER_USER,
           pass: process.env.EMAIL_SERVER_PASSWORD
