cf08c21c352bf1d3f0afd0515229c82b474eba79
pedro <pedrozav@gmail.com>
2023-09-22 11:56:54 -0400

    Update search.ts

--- src/server/api/routers/search.ts
+++ src/server/api/routers/search.ts
@@ -1,11 +1,11 @@
-//import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { z } from "zod";
 import {
   createTRPCRouter,
   publicProcedure,
 } from "~/server/api/trpc";
 //import { PrismaClient } from "../../../../prisma/generated/prisma-client-js";
-import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
 export const searchRouter = createTRPCRouter({
