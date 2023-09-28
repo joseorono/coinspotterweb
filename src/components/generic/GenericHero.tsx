e0d2e76c88b8012d6bae05d382062691c1cf7f39
José Miguel Oroño <joseomaker@gmail.com>
2023-09-27 23:20:10 -0500

    GenericHero: Add backdrop

--- src/components/generic/GenericHero.tsx
+++ src/components/generic/GenericHero.tsx
@@ -3,9 +3,11 @@ import React, { FC } from 'react';
 interface GenericHeroProps {
   PageTitle: string;
   bgImg?: string;
+  bgOpacity? : number;
 }
 
-const GenericHero: FC<GenericHeroProps> = ({ PageTitle, bgImg }) => {
+const GenericHero: FC<GenericHeroProps> = ({ PageTitle, bgImg, bgOpacity }) => {
+    const opacity = bgOpacity ? bgOpacity : 0.3;
     const containerStyle: React.CSSProperties = {
         background: bgImg ? `url(${bgImg})` : `hsl(var(--b1))`,
         backgroundSize: 'cover',
@@ -13,10 +15,16 @@ const GenericHero: FC<GenericHeroProps> = ({ PageTitle, bgImg }) => {
         backgroundRepeat: 'no-repeat',
     };
 
+    const backdropStyle: React.CSSProperties = {
+        background: `hsla(0,0%,0%,${opacity})`,
+    };
+
   return (
-    <div style={containerStyle} className="max-h-[400px] h-screen flex items-center justify-center">
-      <div className='text-6xl text-center text-secondary-focus'>
-        {PageTitle}
+    <div style={containerStyle} className="relative max-h-[400px] h-screen bg-cover bg-center bg-no-repeat bg-image">
+      <div style={backdropStyle} className='absolute inset-0 flex flex-col items-center justify-center text-white'>
+        <h1 className='text-6xl text-center text-secondary-focus'>
+          {PageTitle}
+        </h1>
       </div>
     </div>
   );
