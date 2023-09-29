1402a538e4736bf448076d11e9177c0526677ce1
Mauricio Garcia <78531537+Maggnoone@users.noreply.github.com>
2023-09-29 18:41:58 -0400

    Add changes in the blog page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -26,19 +26,32 @@ const blogPosts = [
 function Blog() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-semibold mb-4">Blog</h1>
+      <h1 className="mb-4 text-3xl font-semibold">Blog</h1>
       <ul className="space-y-4">
-
         {blogPosts.map((post) => (
-          <li key={post.id}>
-            <Link className="text-blue-600 hover:underline" href={`/blog/${post.id}`}>     
+          // <li key={post.id}>
+          //   <Link className="text-blue-600 hover:underline" href={`/blog/${post.id}`}>
+          //       {post.title}
+          //   </Link>
+          //   <p className="text-gray-600">{post.date}</p>
+          //   <p>{post.excerpt}</p>
+          // </li>
+          <Link key={post.id} href={`/blog/${post.id}`}>
+            <div className="mt-12 flex w-full flex-col items-center">
+              <h3 className="mt-12 flex w-[90%] items-center justify-between rounded-[30px] rounded-bl-none rounded-br-none rounded-tl-[30px] rounded-tr-[30px] bg-[#1f2539] px-12 pt-6 text-2xl font-medium">
                 {post.title}
-            </Link>
-            <p className="text-gray-600">{post.date}</p>
-            <p>{post.excerpt}</p>
-          </li>
+              </h3>
+              <div className="flex w-[90%] justify-between rounded-bl-[25px] rounded-br-[25px] rounded-tl-none rounded-tr-none bg-[#1f2539] p-6  duration-500 ease-in">
+                <div className="flex">
+                  <div className="flex flex-col gap-4 px-6">
+                    <p>{post.excerpt}</p>
+                    <p>{post.date}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Link>
         ))}
-
       </ul>
     </div>
   );
