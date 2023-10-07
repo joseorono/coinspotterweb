753fe957eb983e6682f395d6606965621379073d
Mauricio Garcia <78531537+Maggnoone@users.noreply.github.com>
2023-10-07 11:45:23 -0400

    Redesign of the dashboard

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -38,33 +38,45 @@ export default function Dashboard(
 
   return (
     <AppLayout pageTitle="CoinSpotter">
-      <div className="mt-2 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
-        <BsFillBagFill />
-        {statsData.map((d, k) => {
-          return <SmallStatsCard key={k} {...d} colorIndex={k} />;
-        })}
-      </div>
+      <div className="grid">
+        <div className="m-4 rounded-3xl bg-slate-100">
+          <ul className="m-6 flex flex-row justify-between text-center align-middle text-black md:justify-evenly">
+            <li className="cursor-pointer">Users</li>
+            <li className="cursor-pointer">Places</li>
+            <li className="cursor-pointer">Stats</li>
+            <li className="cursor-pointer">Favorites</li>
+          </ul>
+        </div>
+        <div className="dashboardContent">
+          <div className="mt-2 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+            <BsFillBagFill />
+            {statsData.map((d, k) => {
+              return <SmallStatsCard key={k} {...d} colorIndex={k} />;
+            })}
+          </div>
 
-      <div className="flex items-center justify-center">
-        <div className="mb-10 flex min-w-[992px] flex-row gap-4 space-y-4">
-          <div className="mt-6 w-[20%]">
-            <TitleCard title="Welcome" topMargin="mt-4">
-              <div className="text-lg font-semibold">
-                Hello, {session?.user?.name || "Guest"}!
-              </div>
-            </TitleCard>
-          </div>
-          <div className="mt-10 w-[30%]">
-            <TitleCard title="Number of Users">
-              <div className="text-lg font-semibold">
-                Number of Users: {100}
-              </div>
-            </TitleCard>
-          </div>
-          <div className="mt-10 w-[40%]">
-            <TitleCard title="Favorite Places">
-              <div className="text-lg font-semibold">Favorite Places: {5}</div>
-            </TitleCard>
+          <div className="mb-10 grid place-content-center gap-4 space-y-4 md:flex md:flex-row">
+            <div className="col-span-1 mt-6">
+              <TitleCard title="Welcome" topMargin="mt-4">
+                <div className="text-lg font-semibold">
+                  Hello, {session?.user?.name || "Guest"}!
+                </div>
+              </TitleCard>
+            </div>
+            <div className="col-span-1 mt-10">
+              <TitleCard title="Number of Users">
+                <div className="text-lg font-semibold">
+                  Number of Users: {100}
+                </div>
+              </TitleCard>
+            </div>
+            <div className="col-span-1 mt-10">
+              <TitleCard title="Favorite Places">
+                <div className="text-lg font-semibold">
+                  Favorite Places: {5}
+                </div>
+              </TitleCard>
+            </div>
           </div>
         </div>
       </div>
