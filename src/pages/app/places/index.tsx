b85b49abd85d8db42b8ffbc30eabe73cad3c2719
pedro <pedrozav@gmail.com>
2023-11-08 20:27:24 -0400

    fix places and embed map

--- src/pages/app/places/index.tsx
+++ src/pages/app/places/index.tsx
@@ -1,25 +1,47 @@
 import React, { useState } from 'react';
 import { api } from "~/utils/api";
+import GoogleMapWideEmbed from '../../../components/gmaps/gmapWideEmbed';
+
+type Place = {
+  id: number;
+  name: string;
+  address: string;
+  latitude?: number;
+  longitude?: number;
+};
 
 export default function PlacesIndex() {
-    const [searchQuery, setSearchQuery] = useState('');
-    const searchResults = api.places.searchPlaces.useQuery({ query: searchQuery });
-  
-    const handleSearch = () => {
-      // Realiza la búsqueda aquí al presionar un botón o según tus necesidades
-      // Puedes usar el valor de 'searchQuery' para realizar la búsqueda
-    };
-  
-    return (
-      <div>
-        <h1>Página de Lugares</h1>
+  const [searchQuery, setSearchQuery] = useState('');
+  const searchResults = api.places.searchPlaces.useQuery<{ places: Place[] }>({ query: searchQuery });
+  const [selectedPlace, setSelectedPlace] = useState<Place | null>(null);
+
+  const handleSearch = async () => {
+    try {
+      // Realiza la búsqueda aquí
+      await searchResults.refetch();
+    } catch (error) {
+      console.error('Error al buscar lugares:', error);
+    }
+  };
+
+  const handlePlaceSelect = (place: Place) => {
+    // Al seleccionar un lugar, muestra el mapa si tiene información de latitud y longitud
+    if (place.latitude && place.longitude) {
+      setSelectedPlace(place);
+    }
+  };
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <div style={{ flex: 1 }}>
+        <h1>Página de lugares</h1>
         <input
           type="text"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
         <button onClick={handleSearch}>Buscar</button>
-  
+
         {searchResults.isLoading ? (
           <div>Cargando...</div>
         ) : searchResults.isError ? (
@@ -29,10 +51,10 @@ export default function PlacesIndex() {
             <div>
               <h2>Resultados de la búsqueda:</h2>
               <ul>
-                {searchResults.data.places.map((place) => (
-                  <li key={place.id}>
+                {searchResults.data.places.map((place: Place) => (
+                  <li key={place.id} onClick={() => handlePlaceSelect(place)}>
                     <strong>Nombre:</strong> {place.name},{' '}
-                    <strong>Dirección:</strong> {place.address}                   
+                    <strong>Dirección:</strong> {place.address}
                   </li>
                 ))}
               </ul>
@@ -40,5 +62,16 @@ export default function PlacesIndex() {
           )
         )}
       </div>
-    );
-  }
+
+      {selectedPlace && selectedPlace.latitude && selectedPlace.longitude && (
+        <div style={{ flex: 1 }}>
+          <GoogleMapWideEmbed
+            height={350}
+            latitude={selectedPlace.latitude}
+            longitude={selectedPlace.longitude}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
