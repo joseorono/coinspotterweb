0bbf2a80417423eedbf5581aa610519edf129693
Mauricio Garcia <78531537+Maggnoone@users.noreply.github.com>
2024-01-10 20:25:01 -0400

    Use tRPC endpoints to bring data from database and show it in the blog page

--- src/pages/blog/[articleId].tsx
+++ src/pages/blog/[articleId].tsx
@@ -4,77 +4,83 @@ import { GetServerSideProps } from 'next'; // Import GetServerSideProps
 import type { BlogPost } from '@prisma/client'
 import BsCenteredContainer from '~/components/layout/BsCenteredContainer';
 import PageLayout from '~/components/layout/PageLayout';
+import Image from "next/image";
+import { api } from "~/utils/api";
 
 // This function will be executed on the server side
-export async function getServerSideProps(context: any): Promise<  GetServerSideProps<any> > {
-    // Fetch the article data from your data source using the context.params.articleId
-    const articleId = context.params.articleId as string; // Explicitly specify the type
+export async function getServerSideProps(
+  context: any
+): Promise<GetServerSideProps<any>> {
+  // Fetch the article data from your data source using the context.params.articleId
+  const articleId = context.params.articleId as string; // Explicitly specify the type
 
-    // Currently using hardcoded articleData, but you should fetch it from your database or API.
-    // Replace this placeholder content with your actual article data
+  // Currently using hardcoded articleData, but you should fetch it from your database or API.
+  // Replace this placeholder content with your actual article data
 
-    const articleData = {
-        title: 'La Mejor Ropa Deportiva de la Venta Anual de Nordstrom',
-        author: 'Leroy Jenkins',
-        content: 'Inserta aquí el contenido de texto real...',
-        date: '2 de agosto de 2021',
-    };
+  const articleData = {
+    title: "La Mejor Ropa Deportiva de la Venta Anual de Nordstrom",
+    author: "Leroy Jenkins",
+    content: "Inserta aquí el contenido de texto real...",
+    date: "2 de agosto de 2021",
+  };
 
-    // Pass the articleData as props
-    return {
-        props: {
-            articleData,
-        },
-    };
+  // Pass the articleData as props
+  return {
+    props: {
+      articleData,
+    },
+  };
 }
 
 function Article({ articleData }: any) {
   const router = useRouter();
   const { articleId } = router.query;
+  const { data } = api.blog.getPostById.useQuery({
+    postId: Number(articleId),
+  });
+  console.log(data);
 
   // Fetch the article content based on the `articleId` here
 
-
-
   return (
     <>
-        <PageLayout pageTitle={articleData.title}>
-        <BsCenteredContainer extraClasses='mt-8'>
-            <div className="p-5 mx-auto sm:p-10 md:p-16">
-            <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded">
-                <img
-                src="https://source.unsplash.com/random/480x360"
+      <PageLayout key={data?.id} pageTitle={data?.title}>
+        <BsCenteredContainer extraClasses="mt-8">
+          <div className="mx-auto p-5 sm:p-10 md:p-16">
+            <div className="mx-auto flex max-w-3xl flex-col overflow-hidden rounded">
+              <img
+                src="https://source.unsplash.com/featured/?payments,digital"
                 alt=""
-                className="w-full h-60 sm:h-96 bg-base-100 "
-                />
-                <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md bg-base-200">
+                className="h-60 w-full dark:bg-gray-500 sm:h-96"
+              />
+              <div className="m-4 mx-auto -mt-16 space-y-6 bg-base-200 p-6 pb-12 sm:mx-12 sm:px-10 lg:max-w-2xl lg:rounded-md">
                 <div className="space-y-2">
-                    <a
+                  <a
                     href="#"
                     className="inline-block text-2xl font-semibold sm:text-3xl"
-                    >
-                    {articleData.title}
+                  >
+                    {data?.title}
+                  </a>
+                  <p>
+                    <span className="text-xs font-medium uppercase text-gray-600 dark:text-gray-400">
+                      {data?.published} #{data?.id}
+                    </span>
+                  </p>
+                  <p className="text-xs dark:text-gray-400">
+                    By
+                    <a href="#" className="ml-2 text-xs hover:underline">
+                      {data?.author}
                     </a>
-                    <p>
-                        <span className="text-xs font-medium text-gray-600 uppercase dark:text-gray-400">
-                            {articleData.date} #{articleId}
-                        </span>
-                    </p>
-                    <p className="text-xs dark:text-gray-400">
-                    By 
-                    <a href="#" className="text-xs hover:underline">
-                        {articleData.author}
-                    </a>
-                    </p>
+                  </p>
                 </div>
                 <div className="dark:text-gray-100">
-                    <p>{articleData.content}</p>
+                  <p>{data?.content}</p>
                 </div>
-                </div>
+              </div>
             </div>
-            </div>
+          </div>
         </BsCenteredContainer>
-        </PageLayout>
+      </PageLayout>
     </>
   );
 }
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -5,60 +5,67 @@ import React from 'react';
 import Link from 'next/link';
 import PageLayout from '~/components/layout/PageLayout';
 import BsCenteredContainer from '~/components/layout/BsCenteredContainer';
+import { api } from "~/utils/api";
 
 // Placeholder data for blog posts
-const blogPosts = [
-  {
-    id: 1,
-    title: 'Primer Artículo del Blog',
-    slug: 'primer-articulo-del-blog',
-    date: '2023-08-01',
-    excerpt: 'Este es el primer artículo del blog en nuestro sitio web.',
-  },
-  {
-    id: 2,
-    title: 'Segundo Artículo del Blog',
-    slug: 'segundo-articulo-del-blog',
-    date: '2023-08-05',
-    excerpt: 'Aquí tienes nuestro segundo artículo del blog con contenido interesante.',
-  },
-  // Agrega más artículos del blog según sea necesario
-];
+// const blogPosts = [
+//   {
+//     id: 1,
+//     title: "Primer Artículo del Blog",
+//     slug: "primer-articulo-del-blog",
+//     date: "2023-08-01",
+//     excerpt: "Este es el primer artículo del blog en nuestro sitio web.",
+//   },
+//   {
+//     id: 2,
+//     title: "Segundo Artículo del Blog",
+//     slug: "segundo-articulo-del-blog",
+//     date: "2023-08-05",
+//     excerpt:
+//       "Aquí tienes nuestro segundo artículo del blog con contenido interesante.",
+//   },
+//   // Agrega más artículos del blog según sea necesario
+// ];
 
 function Blog() {
+  const { data } = api.blog.getRecentPosts.useQuery();
+
   return (
     <PageLayout pageTitle="Blog">
-
-    
-    <BsCenteredContainer extraClasses='mt-8'>
-      <h1 className="block text-5xl font-semibold uppercase">Últimos artículos</h1>
-      <div className="">
-        {blogPosts.map((post) => (
-          // <li key={post.id}>
-          //   <Link className="text-blue-600 hover:underline" href={`/blog/${post.id}`}>
-          //       {post.title}
-          //   </Link>
-          //   <p className="text-gray-600">{post.date}</p>
-          //   <p>{post.excerpt}</p>
-          // </li>
-            <div className="flex flex-col mt-12">
+      <BsCenteredContainer extraClasses="mt-8">
+        <h1 className="block text-5xl font-semibold uppercase">
+          Últimos artículos
+        </h1>
+        <div className="">
+          {data?.map((post) => (
+            // <li key={post.id}>
+            //   <Link className="text-blue-600 hover:underline" href={`/blog/${post.id}`}>
+            //       {post.title}
+            //   </Link>
+            //   <p className="text-gray-600">{post.date}</p>
+            //   <p>{post.excerpt}</p>
+            // </li>
+            <div key={post.id} className="mt-12 flex flex-col">
               <Link key={post.id} href={`/blog/${post.id}`}>
-              <h3 className="flex w-[90%] items-center justify-between rounded-[30px] rounded-bl-none rounded-br-none rounded-tl-[30px] rounded-tr-[30px] bg-[#1f2539] px-12 pt-6 text-2xl font-medium">
-                {post.title}
-              </h3>
-              <div className="flex w-[90%] justify-between rounded-bl-[25px] rounded-br-[25px] rounded-tl-none rounded-tr-none bg-[#1f2539] p-6  duration-500 ease-in">
-                <div className="flex">
-                  <div className="flex flex-col gap-4 px-6">
-                    <p>{post.excerpt}</p>
-                    <p>{post.date}</p>
+                <h3 className="flex w-[90%] items-center justify-between rounded-[30px] rounded-bl-none rounded-br-none rounded-tl-[30px] rounded-tr-[30px] bg-[#1f2539] px-12 pt-6 text-2xl font-medium">
+                  {post.title}
+                </h3>
+                <div className="flex w-[90%] justify-between rounded-bl-[25px] rounded-br-[25px] rounded-tl-none rounded-tr-none bg-[#1f2539] p-6  duration-500 ease-in">
+                  <div className="flex">
+                    <div className="flex flex-col gap-4 px-6">
+                      <p>{post.content}</p>
+                      <p>{post.published}</p>
+                    </div>
                   </div>
                 </div>
-              </div>
               </Link>
+              {/* {data?.map((pic) => (
+                <p key={pic.id}>{pic.profile_pic_url}</p>
+              ))} */}
             </div>
-        ))}
-      </div>
-    </BsCenteredContainer>
+          ))}
+        </div>
+      </BsCenteredContainer>
     </PageLayout>
   );
 }
