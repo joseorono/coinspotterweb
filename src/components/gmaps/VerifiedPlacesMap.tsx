a8d7f23f016018eee73edcf005665660a236862a
pedro <109691568+pedrozav78@users.noreply.github.com>
2024-01-12 14:39:29 -0400

    change src image form virifed maps

--- src/components/gmaps/VerifiedPlacesMap.tsx
+++ src/components/gmaps/VerifiedPlacesMap.tsx
@@ -10,12 +10,12 @@ const VerifiedPlacesMap: FC<GoogleMapEmbedProps> = ({
   className = "",
 }) => {
     
-    const apiKey = process.env.CS_GMAPS_API_KEY;
+    const apiKey = process.env.CS_GMAPS_API_KEY;    
 
     return (
         <div className={"google-maps-embed " + className}>          
             <iframe
-                src="https://www.google.com/maps/d/u/0/embed?mid=19BZSDKCKctVKDPkf0n8-Z_RJ1gU&ehbc=2E312F"
+                src="https://www.google.com/maps/d/embed?mid=1q94xGgf9s3a0snwHvhK9rI_uWIHey_Y&ehbc=2E312F&noprof=1"
                 width={width}
                 height={height}
                 style={{ border: 0 }}
